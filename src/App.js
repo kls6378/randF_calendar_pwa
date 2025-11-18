3f9976105065ae7e66ca1e0bcf2e69e8ba72e554
YouRun <kls6378@naver.com>
2025-11-18 19:57:06 +0900

    add routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Routes, Route } from 'react-router-dom';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<h1>여기는 메인 캘린더 뷰</h1>} />
+        <Route path="/login" element={<h1>로그인 페이지</h1>} />
+        <Route path="/register" element={<h1>회원가입 페이지</h1>} />
+        <Route path="/mypage" element={<h1>마이페이지</h1>} />
+        <Route path="/groups" element={<h1>내 그룹 목록</h1>} />
+      </Routes>
     </div>
   );
 }
